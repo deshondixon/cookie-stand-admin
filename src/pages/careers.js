1a33a0f1c31b4d1f611de711f41e48ec8070ae9a
deshondixon <deshondixon@gmail.com>
2023-02-07 18:57:38 -0800

    lab done

---
+++ src/pages/careers.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Careers() {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
+      <h1 className="text-4xl">
+        {" "}
+        Careers Page coming soon
+      </h1>
+      <Link href="/" className="p-4 m-4 text-2xl bg-gray-300 rounded-lg">Home</Link>
+    </div>
+  );
+}
